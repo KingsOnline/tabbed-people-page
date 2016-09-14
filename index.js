3017196849157d1cc515656a8663fa2693370209
sd263 <simondate@yahoo.co.uk>
2016-09-14 11:46:37 +0100

    *Added fullbleed image at top. *Added abillity to load page based upon URL anchor.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-	console.log('helloworld');
+
+	openTab(readUrlAnchor());
+
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
- 
+ 		console.log($(this));
         // Show/Hide Tabs
         $('.tabs ' + currentAttrValue).show().siblings().hide();
  
@@ -10,5 +12,25 @@ $(document).ready(function() {
         $(this).parent('li').addClass('active').siblings().removeClass('active');
  
         e.preventDefault();
+
+        //window.history.pushState('page2', 'Title', '/page2.php');
     });
 });
+
+function openTab(url){
+	if ($(url).length > 0) {
+		console.log($(url));
+   		 $('.tab-content').find(url).show().siblings().hide();
+		$('.tab-links').find(url).parent('li').addClass('active').siblings().removeClass('active');	
+	} else {
+	 	$('.tab-content').find('#tab1').show().siblings().hide();
+		$('.tab-links').find('#tab1').parent('li').addClass('active').siblings().removeClass('active');	
+	}
+}
+
+function readUrlAnchor(){
+	var url = window.location.href;
+	if(url.indexOf('#') == -1) return;
+	var section = url.substring(url.indexOf('#'));
+	return section;
+}
